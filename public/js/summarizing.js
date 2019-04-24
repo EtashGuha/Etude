994a7e3ffb426cf679cf0147775e3f569f50b7e4
Ali <alimirz@gatech.edu>
2019-04-24 14:05:06 -0400

    got grobid to take in text
    Former-commit-id: b897291a3e121aa2a9b1cd0a8b1718333a8e836b
    Former-commit-id: 33d578ae30c7ef925451dea82b2c40bfa184cfb1

--- public/js/summarizing.js
+++ public/js/summarizing.js
@@ -1,5 +1,10 @@
 const { dialog } = require('electron').remote;
 const path = require('path');
+const fs = require('fs');
+var mkdirp = require('mkdirp'),
+  request = require('request'),
+  FormData = require('form-data'),
+  async = require('async');
 
 var textData = null;
 var bookmarkArray = [];
@@ -22,6 +27,7 @@ viewerEle.appendChild(iframe);
 filepath = require('electron').remote.getGlobal('sharedObject').someProperty;
    //get text from pdf to send to flask backends
 var PDF_URL  = filepath;
+console.log(PDF_URL);
 var capeClicked = false;
 var btnClicked = false;
 var bookmarkOpened = false;
@@ -60,7 +66,7 @@ function showPDF(pdf_url,bookmark_page) {
                   // Show the first page
                   showPage(bookmark_page);
                   // store.set(i.toString(),pdf_url);
-                  // i++; 
+                  // i++;
     }).catch(function(error) {
                   alert(error.message);
     });
@@ -69,7 +75,7 @@ function showPDF(pdf_url,bookmark_page) {
  function showPage(page_no) {
                 __PAGE_RENDERING_IN_PROGRESS = 1;
                 __CURRENT_PAGE = page_no;
-                
+
                 // Fetch the page
           __PDF_DOC.getPage(page_no).then(function(page) {
                         var __CANVAS = $('.bookmark-canvas').get($(".bookmark-canvas").length-1),
@@ -88,14 +94,14 @@ function showPDF(pdf_url,bookmark_page) {
                                 canvasContext: __CANVAS_CTX,
                                 viewport: viewport
                         };
-    
+
                         // Render the page contents in the canvas
                         page.render(renderContext).then(function() {
                                 __PAGE_RENDERING_IN_PROGRESS = 0;
 
                                 $(".bookmark-canvas").show();
                                 $(".deleteImage_").show();
-                                       
+
                         });
           });
   }
@@ -118,7 +124,7 @@ $(document).on("click",".bookmark-canvas", function(){
   console.log("above you clicked sth");
   document.getElementsByTagName('iframe')[0].contentWindow.document.getElementById('thumbnailView').children[$(this).attr("data") - 1].click()
 });
-    
+
 
 $("#cape_btn").click(function(){
   //get the text and question
@@ -132,7 +138,7 @@ $("#cape_btn").click(function(){
     getSeletedPageText($('#pageRange').val(),$('#topageRange').val());
     capeClicked = true;
   }
-  
+
 })
 
 function getPageText(pageNum, PDFDocumentInstance) {
@@ -219,7 +225,7 @@ function getTextByPage(instance){
         textD = 0;
         capeClicked = true;
       })
-     
+
       return;
     }
   });
@@ -230,12 +236,12 @@ $('#summarizingButton').click(function(){
   console.log("summarizingButtonClicked");
   $('.su_popup').hide();
   getSeletedPageTextForSummarization($('#pageRange').val(),$('#topageRange').val());
-   
+
   // here you can add the loading button
   $('.summarizer_loading').show();
   $('.hover_bkgr_fricc').click(function(){
         $('.hover_bkgr_fricc').hide();
-    });    
+    });
 })
 
 var textDsum = "";
@@ -278,11 +284,171 @@ function getTextByPageForSummarization(instance){
     }else{
       deepai.setApiKey('a5c8170e-046a-4c56-acb1-27c37049b193');
       console.log(textDsum);
-      deepai.callStandardApi("summarization", {
-        text: textDsum}).then((resp) => processSummarizationResult(resp));
+
+      var start;
+      var options = init();
+      start = new Date()
+      processGROBID(options);
+
+
       return;
     }
   });
+}
+
+
+/* Module Require */
+
+// the URL of the GROBID service (to be changed if necessary)
+//const GROBID_URL = "http://localhost:8070/api/";
+
+// for making console output less boring
+const green = '\x1b[32m';
+const red = '\x1b[31m';
+const orange = '\x1b[33m';
+const white = '\x1b[37m';
+const blue = `\x1b[34m`;
+const score = '\x1b[7m';
+const bright = "\x1b[1m";
+const reset = '\x1b[0m';
+
+/**
+ * List all the PDF files in a directory in a synchronous fashion,
+ * @return the list of file names
+ */
+
+function callGROBID(options, file, callback) {
+    console.log("---\nProcessing: " + PDF_URL);
+
+    var form = new FormData();
+    form.append("input", fs.createReadStream(PDF_URL));
+    form.append("consolidateHeader", "1");
+    form.append("consolidateCitations", "0");
+    console.log(typeof fs.createReadStream(options.inPath+"/"+file));
+    var grobid_url = "http://" + options.grobid_host;
+    if (options.grobid_port)
+        grobid_url += ':' + options.grobid_port
+    grobid_url += '/api/';
+    form.submit(grobid_url+options.action, function(err, res, body) {
+        if (err) {
+            console.log(err);
+            return false;
+        }
+
+        if (!res) {
+            console.log("GROBID service appears unavailable");
+            //return false;
+        } else {
+           res.setEncoding('utf8');
+        }
+
+        if (res.statusCode == 503) {
+            // service unavailable, normally it means all the threads for GROBID on the server are currently used
+            // so we sleep a bit before retrying the process
+            sleep.sleep(options.sleep_time);
+            return callGROBID(options, file, callback);
+        } else if (res.statusCode == 204) {
+            // success but no content, no need to read further the response and write an empty file
+            return true;
+        } else if (res.statusCode != 200) {
+            console.log("Call to GROBID service failed with error " + res.statusCode);
+            return false;
+        }
+
+        var body = "";
+        res.on("data", function (chunk) {
+            body += chunk;
+        });
+
+        res.on("end", function () {
+            mkdirp(options.outPath, function(err, made) {
+                // I/O error
+                if (err)
+                    return cb(err);
+
+                // first write the TEI reponse
+                deepai.callStandardApi("summarization", {
+                  text: body}).then((resp) => processSummarizationResult(resp));
+            });
+        });
+    });
+}
+
+/**
+ * Process a PDF file by calling the entity-fishing service and enrich with the resulting
+ * JSON
+ * @param {object} options object containing all the information necessary to manage the paths:
+ *  - {object} inPath input directory where to find the PDF files
+ *  - {object} outPath output directory where to write the results
+ *  - {string} profile the profile indicating which filter to use with the entity-fishing service, e.g. "species"
+ * @return {undefined} Return undefined
+ */
+function processGROBID(options) {
+    // get the PDF paths
+
+    var q = async.queue(function (file, callback) {
+        callGROBID(options, file, callback);
+    }, options.concurrency);
+
+    q.drain = function() {
+        console.log(red, "\nall tasks completed!", reset);
+        end();
+    }
+    q.push("/LaTeX_template_for_preparing_supplementary_material_for_submission_to_Optica.pdf");
+}
+
+/**
+ * Init the main object with paths passed with the command line
+ */
+function init() {
+    var options = new Object();
+
+    // start with the config file
+    const config = require('./config.json');
+    options.grobid_host = config.grobid_host;
+    options.grobid_port = config.grobid_port;
+    options.sleep_time = config.sleep_time;
+
+    // default service is full text processing
+    options.action = "processFulltextDocument";
+    options.concurrency = 10; // number of concurrent call to GROBID, default is 10
+    var attribute; // name of the passed parameter
+    options.inPath = "C:/Users/alimi/Downloads"
+    options.outPath = "C:/Users/alimi/Downloads"
+
+    console.log("\nGROBID service: ", red, options.action+"\n", reset);
+
+    if (!options.inPath) {
+        console.log("Input path is not defines");
+        return;
+    }
+
+    // check the input path
+    fs.lstat(options.inPath, (err, stats) => {
+        if (err)
+            console.log(err);
+        if (stats.isFile())
+            console.log("Input path must be a directory, not a file");
+        if (!stats.isDirectory())
+            console.log("Input path is not a valid directory");
+    });
+
+    // check the output path
+    if (options.outPath) {
+        fs.lstat(options.outPath, (err, stats) => {
+            if (err)
+                console.log(err);
+            if (stats.isFile())
+                console.log("Output path must be a directory, not a file");
+            if (!stats.isDirectory())
+                console.log("Output path is not a valid directory");
+        });
+    }
+    return options;
+}
+
+function end() {
+    console.info('Execution time')
 }
 
 $('#getRangeButton').click(function(){
