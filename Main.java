b9ac3791c37cc404565c8db5b5277831d39a7b2d
Etash Guha <etashguha@Etashs-MacBook-Pro.local>
2019-05-26 15:47:41 -0400

    relativized path and added gitignore

---
+++ Main.java
@@ -0,0 +1,9 @@
+import p1.Calculator;
+public class Main {
+  public static void main(String[] args) {
+  	System.out.println("HELLO");
+    Calculator calc = new Calculator();
+    calc.add();
+    calc.add();
+  }
+}
