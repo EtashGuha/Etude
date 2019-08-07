f9fc6e12de2024f4a355f03ba8c4c15351aa2e5c
Ali <alimirz@gatech.edu>
2019-08-06 23:39:12 -0400

    .

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ locateJavaHome.default({
   ///////////////////////////////////////mainWindow.setMenu(null)
 
   // Open the DevTools.
- mainWindow.webContents.openDevTools()
+ //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
