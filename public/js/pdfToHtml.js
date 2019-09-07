adc8a7320080a7072b6103960c8146dc40709aeb
Etash Guha <etashguha@Etashs-MacBook-Pro.local>
2019-09-07 19:58:57 -0400

    working pdftohtml.jar

--- public/js/pdfToHtml.js
+++ public/js/pdfToHtml.js
@@ -11,18 +11,16 @@ console.log(osvers)
 var textData = null;
 var bookmarkArray = [];
 var Worker = require("tiny-worker");
+
+var jre = require('node-jre');
+
+
+
 onmessage = function pdfToHtml(input) {
-	console.log("Herar")
-	console.log(typeof(input.data[0]))
-	var exec = require('child_process').exec,
-		child;
-	const etudeFilepath = input.data[2];
-	console.log("MY etude file path " + etudeFilepath)
 	var unpackedDirectory = etudeFilepath.replace("app.asar", "app.asar.unpacked")
 	var filenamewithextension = path.parse(input.data[0]).base;
 	filenamewithextension = filenamewithextension.split('.')[0];
 	console.log(filenamewithextension)
-	//update directory to JAR file
 	var pathOfFile = input.data[1] + '/tmp/' + filenamewithextension + '.html'
 	try {
 		if (fs.existsSync(pathOfFile)) {
@@ -33,24 +31,15 @@ onmessage = function pdfToHtml(input) {
 	} catch (err) {
 		console.error(err)
 	}
-	var executionstring;
-	if (osvers == "win32") {
-		executionstring = '\"%ProgramFiles%/Java/jdk-11.0.1/bin/java\" -jar \"' + unpackedDirectory + '/PDFToHTML.jar\" \"' + input.data[0] + '\" \"' + input.data[1] + '/tmp/' + filenamewithextension + '.html\"';
-		console.log(executionstring)
-	} else {
-		console.log("mac")
-		executionstring = 'java -jar ' + unpackedDirectory + '/PDFToHTML.jar \"' + input.data[0] + '\" \"' + input.data[1] + '/tmp/' + filenamewithextension + '.html\"';
-	}
-	console.log(executionstring)
-	child = exec(executionstring,
-		function(error, stdout, stderr) {
-			console.log("nneare")
-			console.log(error)
-			postMessage("done")
-			if (error != null) {
-				console.log("done")
-				console.log('exec error: ' + error);
-				postMessage("done")
-			}
-		});
+	console.log(input.data[1] + '/tmp/ ' + filenamewithextension + '.html')
+	console.log("trying")
+	var output = jre.spawnSync( // call synchronously
+		[unpackedDirectory + '/PDFToHTML.jar'], // add the relative directory 'java' to the class-path
+		'org.fit.pdfdom.PDFToHTML', // call main routine in class 'Hello'
+		[input.data[0], input.data[1] + '/tmp/ ' + filenamewithextension + '.html'], // pass 'World' as only parameter
+		{
+			encoding: 'utf8'
+		} // encode output as string
+	).stdout.trim(); // take output from stdout as trimmed String
+	postMessage("done")
 };
