1a3a78aafff04ddcad3cde718214d10782a3286c
Etash Guha <etashguha@Etashs-MacBook-Pro.local>
2019-09-07 20:06:00 -0400

    fixing bug

--- public/js/pdfToHtml.js
+++ public/js/pdfToHtml.js
@@ -17,10 +17,12 @@ var jre = require('node-jre');
 
 
 onmessage = function pdfToHtml(input) {
+	const etudeFilepath = input.data[2];	
 	var unpackedDirectory = etudeFilepath.replace("app.asar", "app.asar.unpacked")
 	var filenamewithextension = path.parse(input.data[0]).base;
 	filenamewithextension = filenamewithextension.split('.')[0];
-	console.log(filenamewithextension)
+
+	//update directory to JAR file
 	var pathOfFile = input.data[1] + '/tmp/' + filenamewithextension + '.html'
 	try {
 		if (fs.existsSync(pathOfFile)) {
@@ -31,15 +33,14 @@ onmessage = function pdfToHtml(input) {
 	} catch (err) {
 		console.error(err)
 	}
-	console.log(input.data[1] + '/tmp/ ' + filenamewithextension + '.html')
+
+	console.log(input.data[1] + '/tmp/ '+ filenamewithextension + '.html')
 	console.log("trying")
-	var output = jre.spawnSync( // call synchronously
-		[unpackedDirectory + '/PDFToHTML.jar'], // add the relative directory 'java' to the class-path
-		'org.fit.pdfdom.PDFToHTML', // call main routine in class 'Hello'
-		[input.data[0], input.data[1] + '/tmp/ ' + filenamewithextension + '.html'], // pass 'World' as only parameter
-		{
-			encoding: 'utf8'
-		} // encode output as string
-	).stdout.trim(); // take output from stdout as trimmed String
+	var output = jre.spawnSync(  // call synchronously
+	    [unpackedDirectory+'/PDFToHTML.jar'],                // add the relative directory 'java' to the class-path
+	    'org.fit.pdfdom.PDFToHTML',                 // call main routine in class 'Hello'
+	    [input.data[0],input.data[1] + '/tmp/ '+ filenamewithextension + '.html'],               // pass 'World' as only parameter
+	    { encoding: 'utf8' }     // encode output as string
+  	).stdout.trim();           // take output from stdout as trimmed String
 	postMessage("done")
 };
