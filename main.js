8c4a24e31f6334baa7228d157ccfd08cc6ce5702
Ali <alimirz@gatech.edu>
2019-09-24 20:51:47 -0400

    fixed etashs mistake

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function createWindow() {
     autoUpdater.on('download-progress', (progressObj) => {
         let log_message = "Download speed: " + progressObj.bytesPerSecond;
         log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-        log_message = log_message + ' (' + pogressObj.transferred + "/" + progressObj.total + ')';
+        log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
         sendStatusToWindow(log_message);
     })
     autoUpdater.on('update-downloaded', (info) => {
