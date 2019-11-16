b9533fb35d80f4ebe8d31f901c95563771e019d9
Etash Guha <etashguha@Etashs-MacBook-Pro.local>
2019-11-15 19:12:50 -0500

    opening multiwindow mac

--- main.js
+++ main.js
@@ -70,8 +70,71 @@ app.on('will-finish-launching', function() {
 
         currpathtofile = path;
         sharedObject.someProperty = path;
-        mainWindow.loadFile('summarizing.html')
-    });
+        let framebool = true;
+    if (process.platform == 'win32') {
+        framebool = false;
+    }
+    const {
+        width,
+        height
+    } = electron.screen.getPrimaryDisplay().workAreaSize
+    mainWindow = new BrowserWindow({
+        height: height,
+        width: width,
+        minWidth: 600,
+        minHeight: 200,
+        frame: framebool,
+        backgroundColor: '#ffffff',
+        webPreferences: {
+            nodeIntegration: true
+        },
+        icon: 'assets/images/logo.jpg',
+        })
+
+        autoUpdater.checkForUpdatesAndNotify();
+
+        function sendStatusToWindow(text) {
+            mainWindow.webContents.send('message', text);
+        }
+        autoUpdater.on('checking-for-update', () => {
+            sendStatusToWindow('Checking for update...');
+        })
+        autoUpdater.on('update-available', (info) => {
+            sendStatusToWindow('Update available.');
+        })
+        autoUpdater.on('update-not-available', (info) => {
+            sendStatusToWindow('Update not available.');
+        })
+        autoUpdater.on('error', (err) => {
+            sendStatusToWindow('Error in auto-updater. ' + err);
+        })
+        autoUpdater.on('download-progress', (progressObj) => {
+            let log_message = "Download speed: " + progressObj.bytesPerSecond;
+            log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+            log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+            sendStatusToWindow(log_message);
+        })
+        autoUpdater.on('update-downloaded', (info) => {
+            sendStatusToWindow('Update downloaded');
+            autoUpdater.quitAndInstall();
+        });
+        mainWindow.loadFile('splash.html')
+        if (!fs.existsSync(userDataPath + "/tmp")) {
+            fs.mkdirSync(userDataPath + "/tmp")
+        }
+        if (!fs.existsSync(userDataPath + "/folderForHighlightedPDF")) {
+            fs.mkdirSync(userDataPath + "/folderForHighlightedPDF")
+        }
+        console.log("Printing out window")
+        console.log(mainWindow)
+        console.log(mainWindow.location)
+        //This is google analytics stuff
+        analyti.pageview('http://etudereader.com', '/home', 'Example').then((response) => {
+            return response;
+        });
+        mainWindow.webContents.openDevTools()
+            mainWindow.loadFile('summarizing.html')
+        });
 });
 
 ipcMain.on('get-file-data', function(event) {
