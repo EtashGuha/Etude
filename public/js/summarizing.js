4b7519532109a3b2d8b8e848f5f6d3d9c1a271bd
Shlok Natarajan <shlok.natarajan@gmail.com>
2019-12-12 19:39:09 -0500

    Enter to highlight

--- public/js/summarizing.js
+++ public/js/summarizing.js
@@ -275,6 +275,17 @@ $("#etudeButton").click(function() {
 
 })
 
+var searchbox = document.getElementById("topageRange");
+// Execute a function when the user releases a key on the keyboard
+searchbox.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    // Trigger the button element with a click
+    document.getElementById("summarizingButton").click();
+  }
+});
+
 //summarization function
 $('#summarizingButton').click(function() {
 	$('.su_popup').hide();
