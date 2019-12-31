0af32aa20c8b61053d00d3c6579a725acbef7ccd
Ali <alimirz@gatech.edu>
2019-12-31 12:28:15 -0500

    hooked up etash search to etude

--- public/js/kernel.js
+++ public/js/kernel.js
@@ -1,24 +1,16 @@
 const os = require('os')
+const ex = require('C:/Users/alimi/Downloads/EtudeXML/Software/beta8/Etude/public/js/ex')
 var osvers = os.platform()
 
 var Worker = require("tiny-worker");
 
 onmessage = function findTextAnswer(input) {
-	var request = require("request");
-	console.log(input)
-	var options = {
-		method: 'POST',
-		url: 'http://159.89.39.148:8080',
-		formData: {
-			text: input.data[0],
-			question: input.data[1],
-			format: input.data[3],
-			number: input.data[2]
-		}
-	};
-
-	request(options, function(error, response, body) {
-		if (error) throw new Error(error);
-		postMessage(JSON.parse(body).text);
-	});
+	console.log("at least here dawg")
+	const result = ex.getAnswer(input.data[1], input.data[0])
+	console.log(result)
+	result.then((data)=> {
+		console.log("data")
+		console.log(data)
+		postMessage(data);
+	})
 }
--- public/js/summarizing.js
+++ public/js/summarizing.js
@@ -244,7 +244,8 @@ $("#cape_btn").click(function() {
 			var promiseToAppend = new Promise(function(resolve, reject) {
 				//console.log("beginning promise")
 				kernelWorker.onmessage = function(ev) {
-
+					console.log("got it back")
+					console.log(ev.data)
 					$("#capeResult").empty().append(ev.data[0].match( /[^\.!\?]+[\.!\?]+/g )[0]);
 					updateHighlights(ev.data)
 					//console.log("refreshed");
@@ -256,8 +257,8 @@ $("#cape_btn").click(function() {
 					kernelWorker.terminate()
 					resolve("GOOD")
 				}
-				//console.log("redefined kernelWorker on message")
-				kernelWorker.postMessage([x, $("#questionVal").val(), "8", "Sentence"])
+				console.log("redefined kernelWorker on message")
+				kernelWorker.postMessage([x, $("#questionVal").val()])
 				//console.log("kernel worker put up")
 				//kernel.findTextAnswerSync();
 
@@ -387,26 +388,32 @@ $('#getRangeButton').click(function() {
 
 function updateHighlights(arr){
 	//console.log(sentenceToPage)
-	//console.log(arr)
+	console.log(arr)
 	var searchQueries = ""
 	currArr = []
 	arr.forEach((item, index) => {
-
+		console.log(item)
+		item = item.toLowerCase()
 		var splitWolframAnswers = item.match( /[^\.!\?]+[\.!\?]+/g )
 		for (var i = splitWolframAnswers.length - 1; i >= 0; i--) {
 			var realItem = splitWolframAnswers[i]
+			console.log(realItem)
+			console.log(sentenceToPage)
 			var pageNumForSentence = sentenceToPage[replaceAll(realItem, " ", "")]
+			console.log(pageNumForSentence)
 			if(pageNumForSentence != undefined) {
+				console.log("banana")
 				currArr.push(realItem)
 				realItem = realItem.replace(/[^a-zA-Z ]/g, "")
 				realItem = replaceAll(realItem,"\u00A0", "%3D");
 				realItem = replaceAll(realItem, " ", "%3D")
 				realItem = realItem + "%3D" + pageNumForSentence + "PAGENUM"
 				searchQueries += "%20" + realItem
+				console.log(searchQueries)
 			}
 		}
 	})
-	//console.log(searchQueries)
+	console.log(searchQueries)
 	searchQueries = searchQueries.substring(3)
 	searchQueries = replaceAll(searchQueries, "=", "")
 	searchQueries = replaceAll(searchQueries, "&", "")
@@ -729,7 +736,7 @@ function getTextAfterMap(){
 					tokenizer.setEntry(strings)
 					var stringArr = tokenizer.getSentences();
 					for (var i = stringArr.length - 1; i >= 0; i--) {
-						sentenceToPage[replaceAll(stringArr[i], " ","")] = pageNum
+						sentenceToPage[(replaceAll(stringArr[i], " ","")).toLowerCase()] = pageNum
 					} 
 				}).then(function() {
 					if(pdfdoc.numPages === pageNum) {
